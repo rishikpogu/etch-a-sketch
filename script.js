38117c5e78009c1f529a953457c187f8c7944541
Rishik <142647228+rishikpogu@users.noreply.github.com>
2024-06-05 13:36:45 +0530

    add eventlsitener to change bgcolor on hover

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ for (let i = 0; i<n*n; i++) {
     let hw = 600/n
     div.setAttribute("style", `border: 1px solid black; width: ${hw}px; height: ${hw}px;`);
     container.appendChild(div);
+
+    div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = 'red';
+    })
 }
